d2d458e03935ed0cee376c0def78859c38b26ca3
Rachel Pappalardo <rachelan@live.unc.edu>
2017-06-14 21:18:18 -0400

    Trying not to mess up

--- src/ideas.js
+++ src/ideas.js
@@ -1,3 +1,21 @@
+<div className="App-header"  style={{backgroundColor: 'skyblue'}} >
+  <img src={unclogo} style={{width: 170, height: 100}} />
+
+
+
+
+</div>
+
+<hr/>
+<ul className="header">
+  <button class="btn btn-default btn-lg btn-block"><Link to="/" style={{ textDecoration: 'none' }}>Home</Link></button>
+       <button class="btn btn-default btn-lg btn-block"><Link to="/Square4" style={{ textDecoration: 'none' }}>4-Square</Link></button>
+       <button  class="btn btn-default btn-lg btn-block"><Link to="/Square9" style={{ textDecoration: 'none' }}>9-Square</Link></button>
+       <button class="btn btn-default btn-lg btn-block"><Link to="/Square36" style={{ textDecoration: 'none' }}>36-Square</Link></button>
+</ul>
+
+----------------------------------
+
 <div class="btn-group btn-group-justified">
        <button class="btn btn-default btn-lg btn-block"><Link to="/Square4" style={{ textDecoration: 'none' }}>4-Square</Link></button>
        <button  class="btn btn-default btn-lg btn-block"><Link to="/Square9" style={{ textDecoration: 'none' }}>9-Square</Link></button>
