bf32bb27203703a87bfd8c3c148c934357d7a010
Rachel Pappalardo <rachelan@live.unc.edu>
2017-06-29 14:30:13 -0400

    Fixed do1 to do

--- src/App.js
+++ src/App.js
@@ -49,11 +49,6 @@ import {
   Link
 } from 'react-router-dom'
 
-//function sayWord() {
-//  var msg = new SpeechSynthesisUtterance('like');
-//  msg.lang = 'en-US';
-//  speechSynthesis.speak(msg);
-//}
 function sayWord1() {
   var msg = new SpeechSynthesisUtterance('like');
   msg.lang = 'en-US';
@@ -131,7 +126,7 @@ function sayWord15() {
   speechSynthesis.speak(msg);
 }
 function sayWord16() {
-  var msg = new SpeechSynthesisUtterance('do1');
+  var msg = new SpeechSynthesisUtterance('do');
   msg.lang = 'en-US';
   speechSynthesis.speak(msg);
 }
@@ -186,7 +181,7 @@ function sayWord26() {
   speechSynthesis.speak(msg);
 }
 function sayWord27() {
-  var msg = new SpeechSynthesisUtterance('in1');
+  var msg = new SpeechSynthesisUtterance('in');
   msg.lang = 'en-US';
   speechSynthesis.speak(msg);
 }
@@ -235,169 +230,364 @@ function sayWord36() {
   msg.lang = 'en-US';
   speechSynthesis.speak(msg);
 }
-
-
-
-
-
 const home = () => (
-  <div id = "home">
-  <center>
-    <div>
-    <h2 style={{backgroundColor:'skyblue'}}>Universal Core Communication Systems</h2>
-      <img src={unclogo} style={{width: 170, height: 100}} />
-</div>
-<table>
-  <tbody>
-  <tr>
-<td><h5>4 Square Universal Core Communication Book</h5><button>
-  <Link to="/Square4" style={{ textDecoration: 'none' }}><img src={SquareNum4} /></Link>
-</button></td>
-{"\n"}
-<td><h5>9 Location Universal Core Communication Book</h5><button>
-  <Link to="/Square9" style={{textDecoration: 'none'}}><img src={SquareNum9} /></Link>
-</button> </td>
-{"\n"}
-<td><h5>36 Location Universal Core Communication Board</h5><button>
-  <Link to="/Square36" style={{textDecoration: 'none'}}><img src={SquareNum36} /></Link>
-</button></td>
-</tr>
-</tbody>
-</table>
-</center>
-    </div>
-)
-
-
-
-// first section
-const Square4 = () => (
-  <div id ="div1">
-<center>
-    <table>
-      <tbody>
-  <tr>
-<td><button onClick={sayWord1} tabIndex="1"><img src={like}/></button></td>
-  <td><button onClick={sayWord2} tabIndex="2"><img src={want}/></button></td>
-  </tr>
-  <tr>
-    <td><button onClick={sayWord3} tabIndex="3"><img src={not}/></button></td>
-      <td><button onClick={sayWord4} tabIndex="4"><img src={go}/></button></td>
-</tr>
-</tbody>
-</table>
-</center>
-</div>
-)
-
-
-
-//second section
-const Square9 = () => (
-  <div>
-    <center>
-  <table>
-    <tbody>
-<tr>
-  <td><button onClick={sayWord1} tabIndex="1"><img src={like}/></button></td>
-    <td><button onClick={sayWord2} tabIndex="2"><img src={want}/></button></td>
-      <td><button onClick={sayWord5} tabIndex="3"><img src={get}/></button></td>
-</tr>
-<tr>
-  <td><button onClick={sayWord3} tabIndex="4"><img src={not}/></button></td>
-    <td><button onClick={sayWord4} tabIndex="5"><img src={go}/></button></td>
-      <td><button onClick={sayWord6} tabIndex="6"><img src={look}/></button></td>
-</tr>
-<tr>
-  <td><button onClick={sayWord7} tabIndex="7"><img src={I1}/></button></td>
-    <td><button onClick={sayWord8} tabIndex="8"><img src={he}/></button></td>
-      <td><button onClick={sayWord9} tabIndex="10"><img src={open}/></button></td>
-</tr>
-</tbody>
-  </table>
-</center>
-</div>
-)
-
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-    </div>
-)
-
-
-//third section
-const Square36 = ({  }) => (
-  <div>
-<center>
-      <table>
-        <tbody>
-      <tr>
-        <td><button onClick={sayWord1}><img src={like}/></button></td>
-          <td><button onClick={sayWord2}><img src={want}/></button></td>
-            <td><button onClick={sayWord5}><img src={get}/></button></td>
-              <td><button onClick={sayWord10}><img src={make}/></button></td>
-                <td><button onClick={sayWord11}><img src={good}/></button></td>
-                  <td><button onClick={sayWord12}><img src={more}/></button></td>
-</tr>
-<tr><td><button onClick={sayWord3}><img src={not}/></button></td>
-  <td><button onClick={sayWord4}><img src={go}/></button></td>
-    <td><button onClick={sayWord6}><img src={look}/></button></td>
-      <td><button onClick={sayWord13}><img src={turn}/></button></td>
-        <td><button onClick={sayWord14}><img src={help}/></button></td>
-          <td><button onClick={sayWord15}><img src={different}/></button></td>
-</tr>
-<tr><td><button onClick={sayWord7}><img src={I1}/></button></td>
-  <td><button onClick={sayWord8}><img src={he}/></button></td>
-    <td><button onClick={sayWord9}><img src={open}/></button></td>
-      <td><button onClick={sayWord16}><img src={do1}/></button></td>
-        <td><button onClick={sayWord17}><img src={put}/></button></td>
-          <td><button onClick={sayWord18}><img src={same}/></button></td>
-</tr>
-<tr>
-    <td><button onClick={sayWord19}><img src={you}/></button></td>
-      <td><button onClick={sayWord20}><img src={she}/></button></td>
-        <td><button onClick={sayWord21}><img src={that}/></button></td>
-          <td><button onClick={sayWord22}><img src={up}/></button></td>
-            <td><button onClick={sayWord23}><img src={all}/></button></td>
-              <td><button onClick={sayWord24}><img src={some}/></button></td>
-</tr>
-<tr>
-    <td><button onClick={sayWord25}><img src={it}/></button></td>
-      <td><button onClick={sayWord26}><img src={here}/></button></td>
-        <td><button onClick={sayWord27}><img src={in1}/></button></td>
-          <td><button onClick={sayWord28}><img src={on}/></button></td>
-            <td><button onClick={sayWord29}><img src={can}/></button></td>
-              <td><button onClick={sayWord30}><img src={finish}/></button></td>
-</tr>
-<tr>
-    <td><button onClick={sayWord31}><img src={where}/></button></td>
-      <td><button onClick={sayWord32}><img src={what}/></button></td>
-        <td><button onClick={sayWord33}><img src={why}/></button></td>
-          <td><button onClick={sayWord34}><img src={who}/></button></td>
-            <td><button onClick={sayWord35}><img src={when}/></button></td>
-              <td><button onClick={sayWord36}><img src={stop}/></button></td>
-</tr>
-</tbody>
-</table>
-</center>
-  </div>
-)
-
-
-//parts put together
-const BasicExample = () => (
-  <Router>
-    <div>
-      <div className="App">
-</div>
-<Route exact path="/" component={home}/>
-  <Route path="/0" component={home}/>
-      <Route path="/Square4" component={Square4}/>
-      <Route path="/Square9" component={Square9}/>
-      <Route path="/Square36" component={Square36}/>
-    </div>
-  </Router>
-)
-export default BasicExample;
+      <div id = "home">
+         <center>
+            <div>
+               <h2 style={{backgroundColor:'skyblue'}}>Universal Core Communication Systems</h2>
+               <img src={unclogo} style={{width: 170, height: 100}} />
+            </div>
+            <table>
+               <tbody>
+                  <tr>
+                     <td>
+                        <h5>4 Square Universal Core Communication Book</h5>
+                        <button>
+                        <Link to="/Square4" style={{ textDecoration: 'none' }}>
+                        <img src={SquareNum4} />
+                        </Link>
+                        </button>
+                     </td>
+                     {"\n"}
+                     <td>
+                        <h5>9 Location Universal Core Communication Book</h5>
+                        <button>
+                        <Link to="/Square9" style={{textDecoration: 'none'}}>
+                        <img src={SquareNum9} />
+                        </Link>
+                        </button>
+                     </td>
+                     {"\n"}
+                     <td>
+                        <h5>36 Location Universal Core Communication Board</h5>
+                        <button>
+                        <Link to="/Square36" style={{textDecoration: 'none'}}>
+                        <img src={SquareNum36} />
+                        </Link>
+                        </button>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </center>
+      </div>
+      )
+      // first section
+      const Square4 = () => (
+      <div id ="div1">
+         <center>
+            <table>
+               <tbody>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord1} tabIndex="1">
+                        <img src={like}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord2} tabIndex="2">
+                        <img src={want}/>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord3} tabIndex="3">
+                        <img src={not}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord4} tabIndex="4">
+                        <img src={go}/>
+                        </button>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </center>
+      </div>
+      )
+      //second section
+      const Square9 = () => (
+      <div>
+         <center>
+            <table>
+               <tbody>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord1} tabIndex="1">
+                        <img src={like}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord2} tabIndex="2">
+                        <img src={want}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord5} tabIndex="3">
+                        <img src={get}/>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord3} tabIndex="4">
+                        <img src={not}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord4} tabIndex="5">
+                        <img src={go}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord6} tabIndex="6">
+                        <img src={look}/>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord7} tabIndex="7">
+                        <img src={I1}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord8} tabIndex="8">
+                        <img src={he}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord9} tabIndex="10">
+                        <img src={open}/>
+                        </button>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </center>
+      </div>
+      )
+      const Topic = ({ match }) => (
+      <div>
+         <h3>{match.params.topicId}</h3>
+      </div>
+      )
+      //third section
+      const Square36 = ({  }) => (
+      <div>
+         <center>
+            <table>
+               <tbody>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord1}>
+                        <img src={like}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord2}>
+                        <img src={want}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord5}>
+                        <img src={get}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord10}>
+                        <img src={make}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord11}>
+                        <img src={good}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord12}>
+                        <img src={more}/>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord3}>
+                        <img src={not}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord4}>
+                        <img src={go}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord6}>
+                        <img src={look}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord13}>
+                        <img src={turn}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord14}>
+                        <img src={help}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord15}>
+                        <img src={different}/>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord7}>
+                        <img src={I1}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord8}>
+                        <img src={he}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord9}>
+                        <img src={open}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord16}>
+                        <img src={do1}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord17}>
+                        <img src={put}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord18}>
+                        <img src={same}/>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord19}>
+                        <img src={you}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord20}>
+                        <img src={she}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord21}>
+                        <img src={that}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord22}>
+                        <img src={up}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord23}>
+                        <img src={all}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord24}>
+                        <img src={some}/>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord25}>
+                        <img src={it}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord26}>
+                        <img src={here}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord27}>
+                        <img src={in1}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord28}>
+                        <img src={on}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord29}>
+                        <img src={can}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord30}>
+                        <img src={finish}/>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <button onClick={sayWord31}>
+                        <img src={where}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord32}>
+                        <img src={what}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord33}>
+                        <img src={why}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord34}>
+                        <img src={who}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord35}>
+                        <img src={when}/>
+                        </button>
+                     </td>
+                     <td>
+                        <button onClick={sayWord36}>
+                        <img src={stop}/>
+                        </button>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </center>
+      </div>
+      )
+      //parts put together
+      const BasicExample = () => (
+      <Router>
+         <div>
+            <div className="App"></div>
+            <Route exact path="/" component={home}/>
+            <Route path="/0" component={home}/>
+            <Route path="/Square4" component={Square4}/>
+            <Route path="/Square9" component={Square9}/>
+            <Route path="/Square36" component={Square36}/>
+         </div>
+      </Router>
+      )
+      export default BasicExample;
